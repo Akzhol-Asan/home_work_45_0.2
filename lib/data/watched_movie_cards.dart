f11a9e116207962a20ea693cfe281816e59cd166
Akzhol Asan <akjolasan75@gmail.com>
2025-04-26 19:57:40 +0600

    added watched movies

--- lib/data/watched_movie_cards.dart
+++ lib/data/watched_movie_cards.dart
@@ -1,3 +1,48 @@
-import 'package:home_work_45_test/widgets/movie_card.dart'; // путь свой напишешь
+import 'package:home_work_45_test/widgets/movie_card.dart';
 
-List<MovieCard> watchedMovieCards = [];
+List<MovieCard> watchedMovieCards = [
+  MovieCard(
+    image:
+        'https://m.media-amazon.com/images/M/MV5BYzFjMzNjOTktNDBlNy00YWZhLWExYTctZDcxNDA4OWVhOTJjXkEyXkFqcGc@._V1_.jpg',
+    title: 'Movie 1',
+    category: 'Category 1',
+    year: '2020',
+    description: 'A great movie!',
+    rating: 5,
+    buttonText: 'Watch this movie',
+    isMovieCardOpen: false,
+  ),
+  MovieCard(
+    image:
+        'https://www.nowtv.now.com/wp-content/uploads/2020/01/BAD-BOYS-RIDE-OR-DIE-mobile.jpg',
+    title: 'Movie 2',
+    category: 'Category 2',
+    year: '2020',
+    description: 'A great movie!',
+    rating: 3,
+    buttonText: 'Watch this movie',
+    isMovieCardOpen: false,
+  ),
+  MovieCard(
+    image:
+        'https://m.media-amazon.com/images/M/MV5BMzIyNTc1NmUtOTBlNS00YzEwLTlkZTMtZjJkMGM2YzNkYmY3XkEyXkFqcGc@._V1_.jpg',
+    title: 'Movie 3',
+    category: 'Category 3',
+    year: '2020',
+    description: 'A great movie!',
+    rating: 1,
+    buttonText: 'Watch this movie',
+    isMovieCardOpen: false,
+  ),
+  MovieCard(
+    image:
+        'https://m.media-amazon.com/images/S/pv-target-images/e900fe5f2785b76c68d73b55e5a13812b85802fab074972bf96990c38077f106.jpg',
+    title: 'Movie 4',
+    category: 'Category 4',
+    year: '2020',
+    description: 'A great movie!',
+    rating: 4,
+    buttonText: 'Watch this movie',
+    isMovieCardOpen: false,
+  ),
+];
